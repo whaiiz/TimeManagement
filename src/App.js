4404cd27f7838436e32ef6d57ad87dd68d097a57
Jaime Reis <jaime_reis18@outlook.pt>
2021-10-23 19:10:44 +0000

    Task list page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar'; 
+import TaskList from './components/TaskList'; 
 
 function App() {
   return (
     <React.Fragment>
       <Navbar></Navbar>
+      <TaskList></TaskList>
     </React.Fragment>
   );
 }
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,51 +1,98 @@
 import React, {useState, useEffect} from 'react'
 import '../styles/work-items-list.css';
+import '../styles/pagination.css';
 
 export default function WorkItemsList() {
     const [workItems, setWorkItems] = useState([]);
-
     const init = () => {
         setWorkItems([
             {
                 name: 'Play',
-                status: 0,
+                status: "ToDo",
                 date: '20/08/2000',
             },            
             {
                 name: 'Fly',
-                status: 1,
+                status: "Active",
                 date: '20/08/2000',
             },            
             {
                 name: 'Programming',
-                status: 2,
+                status: "Done",
                 date: '20/08/2000',
             },
-
+            {
+                name: 'Play',
+                status: "ToDo",
+                date: '20/08/2000',
+            },            
+            {
+                name: 'Fly',
+                status: "Active",
+                date: '20/08/2000',
+            },            
+            {
+                name: 'Programming',
+                status: "Done",
+                date: '20/08/2000',
+            },
+            {
+                name: 'Play',
+                status: "ToDo",
+                date: '20/08/2000',
+            },            
+            {
+                name: 'Fly',
+                status: "Active",
+                date: '20/08/2000',
+            },            
+            {
+                name: 'Programming',
+                status: "Done",
+                date: '20/08/2000',
+            },
         ]);
     }
-
 
     useEffect(() => init(), []);
 
     return (
-        <table className="task-list">
-            <tr>
-                <th>Name</th>
-                <th>Status</th>
-                <th>Date</th>
-            </tr>
-            {
-                workItems.map(w => {
-                    return(
-                        <tr>
-                            <td class="work-item-name">Play</td>
-                            <td class="work-item-status done-status">Done</td>
-                            <td class="work-item-date">20/08/2020</td>
-                        </tr>
-                    )
-                })
-            }
-        </table>
+        <React.Fragment>
+            <section className="work-item-operations">
+                <article className="search-container">
+                    <input className="search-input" type="text" placeholder="Search" />
+                </article>
+                <article class="add-container">
+                    <a href="#add-task" className="add-button">+</a>
+                </article>
+            </section>
+            <table className="task-list">
+                <tr>
+                    <th>Name</th>
+                    <th>Status</th>
+                    <th>Date</th>
+                </tr>
+                {
+                    workItems.map(w => {
+                        return(
+                            <tr>
+                                <td className="task-name">{w.name}</td>
+                                <td className={`task-status ${w.status.toLowerCase()}-status`}>{w.status}</td>
+                                <td class="task-date">{w.date}</td>
+                            </tr>
+                        )
+                    })
+                }
+            </table>
+            <nav class="pagination">
+                <ul class="pagination-items">
+                    <li><a href="#" class="pagination-item selected-item">1</a></li>
+                    <li><a href="#" class="pagination-item">2</a></li>
+                    <li><a href="#" class="pagination-item">3</a></li>
+                    <li><a href="#" class="pagination-item">4</a></li>
+                    <li><a href="#" class="pagination-item">5</a></li>
+                </ul>
+            </nav>
+        </React.Fragment>
     );
 }
