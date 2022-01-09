0afa256a0280c7e14ff1d5c8740805399c6036e9
Jaime Reis <jaime_reis18@outlook.pt>
2022-01-09 21:54:00 -0100

    Created options to complete, revert and delete a task

--- src/components/TaskTable.jsx
+++ src/components/TaskTable.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 import '../styles/task-table.css';
 import { dateTimeToDate } from '../utils/date-converter';
 
-export default function TaskTable({tasks, onTaskClick}) {
+export default function TaskTable({tasks, onTaskClick, deleteCb}) {
+
+    const deleteTask = (e, id) => {
+        console.log('delete');
+        e.stopPropagation();
+    }
+    
+    const revertTaskStatus = (e, id) => {
+        console.log('revert');
+        e.stopPropagation();
+    }
+
+    const completeTask = (e, id) => {
+        console.log('complete');
+        e.stopPropagation();
+    }
 
     return ( tasks.length === 0 ? <h1>No results to show</h1> : 
         <table className="task-table">
@@ -11,6 +26,7 @@ export default function TaskTable({tasks, onTaskClick}) {
                     <th>Name</th>
                     <th>Status</th>
                     <th>Date</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
@@ -19,6 +35,11 @@ export default function TaskTable({tasks, onTaskClick}) {
                         <td className="task-name">{t.name}</td>
                         <td className={`task-status ${t.status.toLowerCase()}-status`}>{t.status}</td>
                         <td className="task-date">{dateTimeToDate(t.dateTime)}</td>
+                        <td className="task-actions">
+                            <i className="fas fa-check-circle" onClick={e => completeTask(e)}></i>
+                            <i className="fas fa-history" onClick={e => revertTaskStatus(e)}></i>
+                            <i className="fas fa-times-circle" onClick={e => deleteTask(e)}></i>
+                        </td>
                     </tr>
                 ))}
             </tbody>
