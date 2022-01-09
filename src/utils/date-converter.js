8782e94bb5a1735ee0259b22585fa88ffd1b9c85
Jaime Reis <jaime_reis18@outlook.pt>
2022-01-09 18:36:14 -0100

    Date time converter fix

--- src/utils/date-converter.js
+++ src/utils/date-converter.js
@@ -1,6 +1,6 @@
 export const dateTimeToDate = (dateTime) => {
     let date = new Date(dateTime);
     return date.getFullYear().toString() + '-' + 
-           (date.getMonth() + 1).toString().padStart(2, '0') + '-' + 
-           date.getDate().toString().padStart(2, '0');
+           (date.getMonth() + 1).toString().padStart(2, '0') + '-' +
+           date.getUTCDate().toString().padStart(2, '0')
 }
