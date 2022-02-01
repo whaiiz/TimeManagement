692761843685aa283840e58c40d9a72f49366159
Jaime Reis <jaime_reis18@outlook.pt>
2022-02-01 07:49:32 -0100

    Login form modified input placeholders

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -8,14 +8,18 @@ export default function LoginForm() {
             <article className="username-container">
                 <label htmlFor="username">Username</label>
                 <input type="text" id="username" className="username" 
-                    placeholder="Type your username" autoComplete="on"/>
+                    placeholder="anonymouse" autoComplete="on"/>
             </article>
             <article className="password-container">
                 <label htmlFor="password">Password</label>
                 <input type="password" id="password" className="username" 
-                    placeholder="Type your password" autoComplete="on"/>
+                    placeholder="$uper$ecurePaASSw0Rd" autoComplete="on"/>
+                <a className="forgot-password" 
+                    href="/ForgotPassword">Forgot password? Click here.</a>
             </article>
             <button className="login-button" type="submit">Log in</button>
+            <a className="register" href="/Register">
+                No account? Click here to register</a>
         </form>
     )
 }
