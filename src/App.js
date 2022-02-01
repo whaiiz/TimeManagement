29125048c49e57feeeb61d253ee5fb30ce66e312
Jaime Reis <jaime_reis18@outlook.pt>
2022-02-01 07:50:51 -0100

    Creation of register and forgot password page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import TaskList from './pages/TaskList'; 
 import Planning from './pages/Planning';
 import Login from './pages/Login';
+import Register from './pages/Register';
+import ForgotPassword from './pages/ForgotPassword';
 import './styles/normalize.css';
 import './styles/common.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
@@ -11,6 +13,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" exact element={<Login/>}/>
+        <Route path="/Register" exact element={<Register/>}/>
+        <Route path="/ForgotPassword" exact element={<ForgotPassword/>}/>
         <Route path="/TaskList" exact element={<TaskList/>}/>
         <Route path="/Planning" element={<Planning/>}/>
       </Routes>
