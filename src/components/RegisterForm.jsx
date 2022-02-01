6c4c938547d1c4226a1913f5d716cf3440566bcf
Jaime Reis <jaime_reis18@outlook.pt>
2022-02-01 07:48:16 -0100

    Register form component fixes

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
+import '../styles/register-form.css'
 
 export default function RegisterForm() {
     return (
-        <form className="">
+        <form className="register-form">
+            <h1>Register</h1>
             <article>
                 <label htmlFor="email">Email</label>
-                <input type="text" id="username" />
+                <input type="text" id="username" placeholder="randomemail@hotmail.com"/>
             </article>
             <article>
                 <label htmlFor='username'>Username</label>
-                <input type="text" id="username" />
+                <input type="text" id="email" placeholder="anonymouse" />
             </article>
             <article>
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" />
+                <input type="password" id="password" placeholder="$uper$ecurePaASSw0Rd" />
             </article>
             <article>
                 <label>Confirm password</label>
-                <input type="password" id="confirmPassword" />
+                <input type="password" id="confirmPassword" placeholder="$uper$ecurePaASSw0Rd" />
             </article>
+            <button className="register-button" type="submit">Register</button>
+            <a className="login" href="/">Already have an account? 
+                Click here to log in</a>
         </form>
     );
 }
