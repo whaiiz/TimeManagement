c809fea92db7c6e94a1f7f0e417c703c96758770
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-02-03 20:34:49 -0100

    Edit ForgotPasswordForm component

--- src/components/ForgotPasswordForm.jsx
+++ src/components/ForgotPasswordForm.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import '../styles/forgot-password-form.css';
 
-export default function ForgotPasswordComponent() {
-    return <div></div>;
+export default function ForgotPasswordForm() {
+    return (
+        <form className="forgot-password-form">
+            <h1>Forgot password</h1>
+            <article>
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" placeholder="randomemail@hotmail.com" />
+            </article>
+            <button className="send-email-button" type="submit">Send email</button>
+            <a className="login" href="/">Go back to login</a>
+        </form>
+    );
 }
