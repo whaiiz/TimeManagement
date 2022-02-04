bd902a485aee6791bf6d9db91ff0db5be769fc2b
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-02-04 20:01:57 -0100

    Create authentication-service.js

---
+++ src/services/authentication-service.js
@@ -0,0 +1,23 @@
+const baseUrl = 'https://localhost:5001/api';
+
+export const handleLoginRequest = (username, password) => {
+    return fetch(baseUrl + '/Authentication/Login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username, password})
+    });
+}
+
+export const handleRegisterRequest = (user) => {
+    return fetch(baseUrl + '/Authentication/Login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    });
+}
