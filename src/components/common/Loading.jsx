0e11f97490167945d0fcb5caade1c19ada497fee
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-02-19 11:23:35 -0100

    Implemented loading when making the register request

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
 import '../../styles/loading.css';
 
-export default class Loading extends Component {
-    render() {
-        return (
-            <div class="loader-wrapper">
-                <span class="loader"><span class="loader-inner"></span></span>  
-            </div>
-        )
-    }
+export default function Loading() {
+    return (
+        <div className="loader-wrapper">
+            <span className="loader"><span className="loader-inner"></span></span>  
+        </div>
+    );
 }
