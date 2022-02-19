f53de58305e257e132d7414f47257801b1e90a4f
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-02-19 11:22:51 -0100

    Implemented loading page in login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import Planning from './pages/Planning';
 import Login from './pages/Login';
 import TaskList from './pages/TaskList';
@@ -9,16 +9,16 @@ import './styles/common.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/Login" exact element={<Login/>}/>
-        <Route path="/Register" exact element={<Register/>}/>
-        <Route path="/ForgotPassword" exact element={<ForgotPassword/>}/>
-        <Route path="/" exact element={<TaskList/>}/>
-        <Route path="/Planning" element={<Planning/>}/>
-      </Routes>
-    </Router>
+    return (
+        <Router>
+            <Routes>
+                <Route path="/Login" exact element={<Login/>}/>
+                <Route path="/Register" exact element={<Register/>}/>
+                <Route path="/ForgotPassword" exact element={<ForgotPassword/>}/>
+                <Route path="/" exact element={<TaskList/>}/>
+                <Route path="/Planning" element={<Planning/>}/>
+            </Routes>
+        </Router> 
   );
 }
 
--- src/components/authentication/LoginForm.jsx
+++ src/components/authentication/LoginForm.jsx
@@ -5,7 +5,7 @@ import * as yup from "yup";
 import { useForm } from 'react-hook-form';
 import { successMessage, errorMessage } from '../../utils/sweet-alert';
 import { login } from '../../business-layer/authentication';
-import { Loading } from '../../components/common/Loading';
+import Loading from '../../components/common/Loading';
 
 export default function LoginForm() {
     const [isPageLoading, setIsPageLoading] = useState(false);
@@ -25,35 +25,38 @@ export default function LoginForm() {
 
     const handleLogin = async _ => {
         let { username, password } = getValues();
+
+        setIsPageLoading(true);
+
         let { isLoggedIn, message } = await login(username, password);
         
-        setIsPageLoading(true);
+        setIsPageLoading(false);
 
         if (isLoggedIn) successMessage('Success', message).then(_ => window.location.href = '/');
         else errorMessage('Error', message);
-
-        setIsPageLoading(false);
     }
 
     return (
-        isPageLoading  ? <Loading></Loading> :  
-        <form className="login-form">
-            <h1 className="login-title">Login</h1>
-            <article className="username-container">
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" className="username" {...register("username")}
-                    placeholder="anonymouse" autoComplete="on"/>
-                <label className="error-text">{errors.username?.message}</label>
-            </article>
-            <article className="password-container">
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" className="password" {...register("password")}
-                    placeholder="$uper$ecurePaASSw0Rd" autoComplete="on"/>
-                <a className="forgot-password" href="/ForgotPassword">Forgot password? Click here.</a>
-                <label className="error-text">{errors.password?.message}</label>
-            </article>
-            <button className="login-button" type="submit" onClick={handleSubmit(handleLogin)}>Log in</button>
-            <a className="register" href="/Register">No account? Click here to register.</a>
-        </form>
-    )
+        <React.Fragment>
+            { isPageLoading ? <Loading /> : "" }  
+            <form className="login-form">
+                <h1 className="login-title">Login</h1>
+                <article className="username-container">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" className="username" {...register("username")}
+                        placeholder="anonymouse" autoComplete="on"/>
+                    <label className="error-text">{errors.username?.message}</label>
+                </article>
+                <article className="password-container">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" className="password" {...register("password")}
+                        placeholder="$uper$ecurePaASSw0Rd" autoComplete="on"/>
+                    <a className="forgot-password" href="/ForgotPassword">Forgot password? Click here.</a>
+                    <label className="error-text">{errors.password?.message}</label>
+                </article>
+                <button className="login-button" type="submit" onClick={handleSubmit(handleLogin)}>Log in</button>
+                <a className="register" href="/Register">No account? Click here to register.</a>
+            </form>
+        </React.Fragment>
+    );
 }
