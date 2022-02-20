1bd9e27e0c99ec60b3a583c246f292e4e8228fbb
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-02-20 21:07:01 -0100

    Added route for password reset

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import TaskList from './pages/TaskList';
 import Register from './pages/Register';
 import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
 import './styles/normalize.css';
 import './styles/common.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
@@ -15,6 +16,7 @@ function App() {
                 <Route path="/Login" exact element={<Login/>}/>
                 <Route path="/Register" exact element={<Register/>}/>
                 <Route path="/ForgotPassword" exact element={<ForgotPassword/>}/>
+                <Route path="/ResetPassword" exact element={<ResetPassword/>}/>
                 <Route path="/" exact element={<TaskList/>}/>
                 <Route path="/Planning" element={<Planning/>}/>
             </Routes>
