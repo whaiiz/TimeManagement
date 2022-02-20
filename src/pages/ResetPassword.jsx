74b5208bcbf355cad54ad11e41813d0e540d80ce
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-02-20 21:06:34 -0100

    Creation of reset password page

---
+++ src/pages/ResetPassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ResetPasswordForm from '../components/authentication/ResetPasswordForm';
+import '../styles/reset-password.css';
+
+export default function ResetPassword() {
+    return (
+        <section className="reset-password-container">
+            <ResetPasswordForm/>
+        </section>	
+    )
+}
