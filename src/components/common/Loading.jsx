469d5dedb6c74dfaf82b9093408fb3b5cb713868
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-02-22 20:43:30 -0100

    loading style fixed

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import '../../styles/loading.css';
 
 export default function Loading() {
     return (
-        <div className="loader-wrapper">
-            <span className="loader"><span className="loader-inner"></span></span>  
-        </div>
+        <main className = "loading">
+            <div className="loader-wrapper">
+                <span className="loader"><span className="loader-inner"></span></span>  
+            </div>
+        </main>
     );
 }
