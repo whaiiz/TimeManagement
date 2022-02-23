83a000b1bf65ac92621cb2bd7a31ba54d1221a86
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-02-23 21:10:54 -0100

    Organized css files in folders

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -13,7 +13,13 @@ export default function Navbar() {
         <nav className="menu">
             <ul className="navbar-items">
                 <li>
-                    <Link to='/' className="task-link">
+                    <Link to='/' className="today-link">
+                        <i className="fas fa-clock today-icon"></i>
+                    </Link>
+                    <span className="today-tooltip">Today</span>
+                </li>
+                <li>
+                    <Link to='/Tasks' className="task-link">
                         <i className="fas fa-tasks task-icon"></i>
                     </Link>
                     <span className="task-tooltip">Tasks</span>
@@ -28,7 +34,7 @@ export default function Navbar() {
                     <span onClick={handleLogout} className="logout-link">
                         <i className="fas fa-door-open logout-icon"></i>
                     </span>
-                    <span className="calendar-tooltip">Log out</span>
+                    <span className="logout-tooltip">Log out</span>
                 </li>
             </ul>
         </nav>
