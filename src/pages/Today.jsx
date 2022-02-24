9e66da64aed1e0e5db7784470792a5efd60366f5
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-02-24 21:17:00 -0100

    timer base

--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Navbar from '../components/common/Navbar'
 import TimerComponent from '../components/tasks/Timer'
 import TodayTasksList from '../components/tasks/TodayTaskList'
+import { getTasks} from '../business-layer/tasks';
+import { getUserLoggedInToken } from  '../business-layer/authentication';
 import '../styles/pages/today.css'
 
 export default function Today() {
+    const [todaysTasks, setTodaysTasks] = useState([]);
+
+    useEffect(() => {
+        if (!getUserLoggedInToken()) window.location.href = '/Login';
+
+        getTasks().then(r => {
+            if(r.status === 200) {
+                setTodaysTasks(r.tasks);
+            }
+        });
+    }, [setTodaysTasks])
+    
     return(
         <React.Fragment>
             <Navbar />
             <TimerComponent />
-            <TodayTasksList />
+            <TodayTasksList todaysTasks = {todaysTasks} />
         </React.Fragment>
     );
 }
