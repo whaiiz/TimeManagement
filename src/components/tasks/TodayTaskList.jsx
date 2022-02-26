6940e70a7db37d405f741f7ad22f02f18f2d9b63
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-02-26 21:30:24 -0100

    Timer semantic

--- src/components/tasks/TodayTaskList.jsx
+++ src/components/tasks/TodayTaskList.jsx
@@ -5,20 +5,27 @@ import '../../styles/components/tasks/today-task-list.css'
 export default function TodayTasksList() {
     const [todayTasks, setTodayTasks] = useState([]);
     
+    const completeTask = (id) => {
+        console.log(id); 
+    }
+
     useEffect(() => {
         getTasks().then(response => {
+            console.log(response);
             if (response.success) setTodayTasks(response.tasks);
         });
     }, [])
     
-    
     return (
         <React.Fragment>
             <ul>
-                {todayTasks.map(t => {
-                    <li>
-                        
-                    </li>       
+                {todayTasks.map((t, i) => {
+                    return (
+                        <li key={i}>
+                            <label>{t.name}</label>
+                            {t.status === 'Active' && <i className="fas fa-check-circle" onClick={e => completeTask(e, t.id)}></i>}
+                        </li>
+                    )       
                 })}
             </ul>
         </React.Fragment>
