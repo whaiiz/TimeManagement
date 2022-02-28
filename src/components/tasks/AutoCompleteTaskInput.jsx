5796ce5f9ea483b0598475ae37d208ef8d829714
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-02-28 22:25:35 -0100

    Create AutoCompleteTaskInput.jsx

---
+++ src/components/tasks/AutoCompleteTaskInput.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import '../../styles/common/auto-complete.css';
+
+export default function AutoCompleteInput({collection, onItemClick, addNewTaskCallback}) {
+
+    const [suggestions, setSuggestions] = useState(collection);
+    const [areSuggestionsVisible, setAreSuggestionsVisible] = useState(false);
+
+    const getSuggestions = ({target : {value}}) => {
+        return collection.filter(t => t.name.toLowerCase().includes(value.toLowerCase()));
+    }
+ 
+    useEffect(() => {
+        setSuggestions(collection)
+    }, [collection])
+
+    return(
+        <form autoComplete="off">
+            <section className="auto-complete">
+                <input type="text" onInput={e => setSuggestions(getSuggestions(e))}
+                                   onClick={_ => setAreSuggestionsVisible(true)}
+                                   onBlur={_ => setTimeout(_ => setAreSuggestionsVisible(false), 100)} />
+                { areSuggestionsVisible && <article className="auto-complete-items">
+                    {
+                        suggestions.map(s => {
+                            return (
+                                <div key={s.id} onClick={_ => onItemClick(s.id)}>
+                                    <label>{s.name}</label>
+                                </div>
+                            )
+                        })
+
+
+
+                    }
+                <div onClick={_ => onItemClick(s.id)}>
+                    <label>{s.name}</label>
+                </div>
+                </article> }
+            </section>
+        </form>
+    )
+}
