3a2fdc436771ed9f8a7262f82026f3e96e4de007
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-03-06 19:57:22 -0100

    Creation of bl to udpateUserDefaultBreakTime and updateUserDefaultFocusTime

--- src/business-layer/user.js
+++ src/business-layer/user.js
@@ -1,9 +1,23 @@
-import { getUserRequest } from '../repositories/user-repository';
-import { getUserLoggedInToken } from '../business-layer/authentication';
+import { getUserRequest, updateUserDefaultFocusTimeRequest, 
+    updateUserDefaultBreakTimeRequest } from '../repositories/user-repository';
 
-export const getUser = async () => {
+export const getUser = async _ => {
     try {
-        let response = await getUserRequest(getUserLoggedInToken());
+        let response = await getUserRequest();
         return await response.json(); 
     } catch(ex) { return null; }
+}
+
+export const updateUserDefaultBreakTime = async breakTime => {
+    try {
+        let response = await updateUserDefaultBreakTimeRequest(breakTime);
+        return response.status === 200;
+    } catch(ex) { return false; }
+}
+
+export const updateUserDefaultFocusTime = async focusTime => {
+    try {
+        let response = await updateUserDefaultFocusTimeRequest(focusTime);
+        return response.status === 200;
+    } catch(ex) { return false; }
 }
