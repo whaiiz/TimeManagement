fffb4fc94d2780ad5cdeb8caf238291aea6469a3
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-03-13 21:05:13 -0100

    Create update-task-date.js

---
+++ src/business-layer/tasks/update-task-date.js
@@ -0,0 +1,34 @@
+import { refreshToken } from '../authentication';
+import { updateTaskDateRequest }  from '../../repositories/task-repository';
+
+const unknownErrorResponse = {
+    success: false
+};
+
+const handleSuccessResponse = async _ => {
+    return {
+        success: true
+    }
+};
+
+const handleUnauthorizedResponse = async (request, id, date) => {
+    let response = await refreshToken();
+    if (response.status !== 200) return { success: false, userNotLoggedIn: true }
+    return await updateTaskDate(id, date);
+}
+
+const handleInternalServerErrorResponse = async _ => unknownErrorResponse;
+
+const responseHandlers = {
+    401 : handleUnauthorizedResponse,
+    200 : handleSuccessResponse,
+    500 : handleInternalServerErrorResponse
+}
+
+export const updateTaskDate = async (id, date) => {
+    try {
+        let request = await updateTaskDateRequest();
+        let handleResponse = responseHandlers[request.status];
+        return handleResponse ? await handleResponse(request, id, date) : unknownErrorResponse;
+    } catch(ex) { return unknownErrorResponse; }
+}
