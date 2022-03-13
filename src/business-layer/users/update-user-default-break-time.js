6f3c8b57c187c6c2eb7fa5a48fa21d95999195ae
Jaime Reis <42680567+whaiiz@users.noreply.github.com>
2022-03-13 21:05:34 -0100

    Create update-user-default-break-time.js

---
+++ src/business-layer/users/update-user-default-break-time.js
@@ -0,0 +1,34 @@
+import { refreshToken } from '../authentication';
+import { updateUserDefaultBreakTimeRequest }  from '../../repositories/user-repository';
+
+const unknownErrorResponse = {
+    success: false
+};
+
+const handleSuccessResponse = async _ => {
+    return {
+        success: true
+    }
+};
+
+const handleUnauthorizedResponse = async (request, breakTime) => {
+    let response = await refreshToken();
+    if (response.status !== 200) return { success: false, userNotLoggedIn: true }
+    return await updateUserDefaultBreakTime(breakTime);
+}
+
+const handleInternalServerErrorResponse = async _ => unknownErrorResponse;
+
+const responseHandlers = {
+    401 : handleUnauthorizedResponse,
+    200 : handleSuccessResponse,
+    500 : handleInternalServerErrorResponse
+}
+
+export const updateUserDefaultBreakTime = async (breakTime) => {
+    try {
+        let request = await updateUserDefaultBreakTimeRequest();
+        let handleResponse = responseHandlers[request.status];
+        return handleResponse ? await handleResponse(request, breakTime) : unknownErrorResponse;
+    } catch(ex) { return unknownErrorResponse; }
+}
